6855caf955a83097f0a3c85804f01f532cae663f
weitian1993gbrsa <weitian1993.gbrsa@gmail.com>
2025-08-21 18:52:15 +0800

    a

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="app-footer">
+      <p>© {new Date().getFullYear()} GB Rope Skipping Academy</p>
+    </footer>
+  );
+}
