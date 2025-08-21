61dcb8fc8ada003a686a3afd7c63e30ac9561488
weitian1993gbrsa <weitian1993.gbrsa@gmail.com>
2025-08-21 22:21:53 +0800

    a

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Footer from "../components/Footer";
+import Footer from "./components/Footer";
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
