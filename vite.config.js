27fd1e7a8fe016aac9481e2f202c16116c55bf0d
weitian1993gbrsa <weitian1993.gbrsa@gmail.com>
2025-08-21 18:45:18 +0800

    a

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  define: {
+    'process.env': process.env
+  }
 })
